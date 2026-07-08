aa2b3d0302710b325a0f870da253a0a42685fe38
kshittiz101 <chaudharykshittiz950@gmail.com>
2026-07-08 22:19:32 +0545

    feat: model is added

--- lab3/main/models.py
+++ lab3/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    post_img = models.ImageField(upload_to="post_imgs/")
+    is_published = models.BooleanField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
