489d94c3166bd0693279f62ce65df91ccd1530ec
kshittiz101 <chaudharykshittiz950@gmail.com>
2026-07-08 22:23:02 +0545

    feat: crud api is added

--- lab3/config/settings.py
+++ lab3/config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third-party apps
+    "rest_framework",
     # internal apps
     "main",
 ]
